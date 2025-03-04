b5f5191bccf6605d345bec08a1374d6b71061e81
bigjohnn1 <vojtaben15@seznam.cz>
2025-03-04 07:43:52 +0100

    moved imrpovised sticky footer logic

--- components/App/Footer.vue
+++ components/App/Footer.vue
@@ -1,41 +1,30 @@
 <template>
-  <div class="flex flex-col min-h-screen">
-    <main class="flex-grow">
-      <slot />
-    </main>
-    <footer
-      ref="footerRef"
-      class="relative bg-gray-900 text-white p-6 text-center overflow-hidden"
-    >
-      <div v-show="isVisible" class="absolute inset-0 opacity-20">
-        <svg class="w-full h-full" xmlns="http://www.w3.org/2000/svg">
-          <rect width="100%" height="100%" fill="url(#stars)" />
-          <defs>
-            <pattern
-              id="stars"
-              x="0"
-              y="0"
-              width="50"
-              height="50"
-              patternUnits="userSpaceOnUse"
-            >
-              <circle cx="10" cy="10" r="1" fill="#E5E7EB" class="twinkle" />
-              <circle
-                cx="40"
-                cy="40"
-                r="2"
-                fill="#E5E7EB"
-                class="twinkle-alt"
-              />
-            </pattern>
-          </defs>
-        </svg>
-      </div>
-      <div class="relative z-10">
-        <p>© {{ year }} Big John</p>
-      </div>
-    </footer>
-  </div>
+  <footer
+    ref="footerRef"
+    class="relative bg-gray-900 text-white p-6 text-center overflow-hidden"
+  >
+    <div v-show="isVisible" class="absolute inset-0 opacity-20">
+      <svg class="w-full h-full" xmlns="http://www.w3.org/2000/svg">
+        <rect width="100%" height="100%" fill="url(#stars)" />
+        <defs>
+          <pattern
+            id="stars"
+            x="0"
+            y="0"
+            width="50"
+            height="50"
+            patternUnits="userSpaceOnUse"
+          >
+            <circle cx="10" cy="10" r="1" fill="#E5E7EB" class="twinkle" />
+            <circle cx="40" cy="40" r="2" fill="#E5E7EB" class="twinkle-alt" />
+          </pattern>
+        </defs>
+      </svg>
+    </div>
+    <div class="relative z-10">
+      <p>© {{ year }} Big John</p>
+    </div>
+  </footer>
 </template>
 
 <script lang="ts" setup>
--- components/VueFrag.vue
+++ components/VueFrag.vue
@@ -1,5 +1,7 @@
 <template>
-  <slot />
+  <div class="flex flex-column min-h-screen">
+    <main class="flex-grow"><slot /></main>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
