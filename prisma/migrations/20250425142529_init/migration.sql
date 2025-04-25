4e2b7caff52f669ebd031f50074002d2396370d6
Big John <vojtaben15@seznam.cz>
2025-04-25 16:27:55 +0200

    prisma schema, init migration

---
+++ prisma/migrations/20250425142529_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "ContactSubmission" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "reason" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "consent" BOOLEAN NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ContactSubmission_pkey" PRIMARY KEY ("id")
+);
