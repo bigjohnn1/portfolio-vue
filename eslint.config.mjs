e3f540b1de68ff4c74c4098d4331d3a39854c3ff
bigjohnn1 <vojtaben15@seznam.cz>
2026-05-07 13:29:25 +0200

    refactor: remove unused ESLint rules and simplify configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,35 +6,6 @@ export default withNuxt({
     'vue/no-v-html': 'off',
     'vue/html-self-closing': 'off',
     'vue/no-use-v-if-with-v-for': 'off',
-    '@typescript-eslint/no-unused-vars': ['warn'],
     'object-shorthand': ['error', 'always'],
-    'import/order': [
-      'error',
-      {
-        groups: [
-          'builtin',
-          'external',
-          'internal',
-          ['sibling', 'parent'],
-          'index',
-          'unknown',
-        ],
-        'newlines-between': 'never',
-        alphabetize: {
-          order: 'asc',
-          caseInsensitive: true,
-        },
-      },
-    ],
-    'sort-imports': [
-      'error',
-      {
-        ignoreCase: false,
-        ignoreDeclarationSort: true,
-        ignoreMemberSort: false,
-        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-        allowSeparatedGroups: true,
-      },
-    ],
   },
 })
